9e5aff9355fb4af9308b1f4943c8dd30c1dca8c8
Miguel Luiz Capristano <71992751+miguel-luiz-cap@users.noreply.github.com>
2021-09-03 22:22:34 -0300

    Adicionando Array e FlatList

--- src/pages/Artista.js
+++ src/pages/Artista.js
@@ -1,29 +1,79 @@
 import * as React from 'react';
-import { ImageBackground, View, Text, StyleSheet } from 'react-native';
+import {
+  ImageBackground,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Artista() {
   return (
     <View style={estilo.container}>
       <Text style={estilo.titulo}>Os melhores artistas</Text>
-      <View style={estilo.artista}>
-        <View>
-          <Text style={estilo.txtArtista}>Zé Ramalho</Text>
-        </View>
+      <FlatList
+        data={Arr_artistas}
+        keyExtractor={(item) => {
+          item.uid.toString();
+        }}
+        renderItem={({ item }) => 
+          <View style={estilo.artista}>
+            <View>
+              <Text style={estilo.txtArtista}> {item.nome} </Text>
+            </View>
 
-        <View style={estilo.rede}>
-          <Text style={estilo.curtidas}>
-            <MaterialCommunityIcons name="thumb-up" size={20} color={"red"}/> 500
-            Curtidas
-          </Text>
-          <Text style={estilo.seguidores}>
-            <MaterialCommunityIcons name="account-heart" size={20} color="#00F" /> 354 Seguidores
-          </Text>
-        </View>
-      </View>
+            <View style={estilo.rede}>
+              <Text style={estilo.curtidas}>
+                <MaterialCommunityIcons
+                  name="thumb-up"
+                  size={20}
+                  color={'red'}
+                />{' '}
+                {item.like} Curtidas
+              </Text>
+              <Text style={estilo.seguidores}>
+                <MaterialCommunityIcons
+                  name="account-heart"
+                  size={20}
+                  color="#00F"
+                />{' '}
+                {item.seguidores} Seguidores
+              </Text>
+            </View>
+          </View>
+        }
+      />
     </View>
   );
 }
+
+const Arr_artistas = [
+  {
+    uid: 1,
+    nome: 'Zé Ramalho',
+    like: 400,
+    seguidores: 500,
+  },
+  {
+    uid: 2,
+    nome: 'Mamomas Assassinas',
+    like: 705,
+    seguidores: 1987,
+  },
+  {
+    uid: 3,
+    nome: 'Raul Seixas',
+    like: 4000,
+    seguidores: 4512,
+  },
+  {
+    uid: 4,
+    nome: 'Engenheiros do Hawai',
+    like: 1313,
+    seguidores: 122,
+  },
+];
 
 const estilo = StyleSheet.create({
   container: {
