0db4f79465f94201ef9e8692f155b576f4b37566
Miguel Luiz Capristano <71992751+miguel-luiz-cap@users.noreply.github.com>
2021-09-24 22:00:43 -0300

    Atualização da aula 24-09-2021

---
+++ navegacaotelas/src/artista/RaulSeixas.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import {
+  ImageBackground,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from 'react-native';
+
+
+export default function RaulSeixas() {
+  return(
+  <View style={estilo.container}>
+    <Text style={estilo.titulo}> Raul Seixas </Text>
+    <ScrollView horizontal = {true}>
+      <View>
+      <Image resizeMode={'stretch'} style={estilo.img} source={ require('../../assets/eng_hawai.png' ) }/>  
+      <Text>Brilhando</Text>
+      </View>
+      <View><Image resizeMode={'stretch'} style={estilo.img} source={ require('../../assets/Piano.png' ) }/>  
+      <Text>Brilhando</Text>
+      </View>
+      <View><Image resizeMode={'stretch'} style={estilo.img} source={ require('../../assets/eng_hawai.png' ) }/>  
+      <Text>Brilhando</Text>
+      </View>
+    </ScrollView>
+
+  </View>
+  );
+}
+
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    //color: '#fff'
+    backgroundColor: '#a4d2f7',
+  },
+
+  titulo: {
+    color: '#FFF',
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginVertical: 30,
+  },
+  artista: {
+    backgroundColor: '#a2f5e9',
+    margin: 15,
+    padding: 5,
+    borderRadius: 10,
+
+    textAlign: 'center',
+  },
+  txtArtista: {
+    fontSize: 20,
+  },
+  rede: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  img: {
+    width: 200,
+    maxHeight: 300,
+    marginHorizontal: 35,
+    marginVertical: 20,
+    borderStyle: '',
+  }
+});
---
+++ navegacaotelas/src/pages/Home.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { ImageBackground, View, Text, StyleSheet } from 'react-native';
+
+
+export default function Home() {
+  return (
+    <View style={estilo.container}>
+      <ImageBackground
+        source={require('../../assets/Piano.png')}
+        style={estilo.fundoimg}
+        resizeMode="cover">
+
+      <Text style={estilo.titulo}>App de Música</Text>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fundoimg: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  titulo: {
+    color: '#FFF',
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
+});
